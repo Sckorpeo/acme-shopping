e7919fe6672f423eefa9038da983d5b69126cc98
Kieran B <kieranbru.web@gmail.com>
2022-08-13 18:41:43 -0400

    Added Rating model

--- server/db/index.js
+++ server/db/index.js
@@ -1,18 +1,20 @@
 const conn = require('./conn');
-const { Sequelize } = conn;
 const Product = require('./Product');
 const User = require('./User');
 const LineItem = require('./LineItem');
 const Order = require('./Order');
+const Rating = require('./Rating');
 
 User.hasMany(Order);
 Order.hasMany(LineItem);
 LineItem.belongsTo(Product);
+Rating.belongsTo(Product);
+Rating.belongsTo(User);
 
 module.exports = {
-  conn,
-  User,
-  Product,
-  LineItem,
-  Order
+    conn,
+    User,
+    Product,
+    LineItem,
+    Order
 };
