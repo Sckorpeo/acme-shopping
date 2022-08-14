971851b8786bd60de5bd34ffa0cb2ef4f03dfea8
Kaiyuan Ma <frankmkmk1@gmail.com>
2022-08-14 15:39:57 -0400

    added login, userpage, logout, and redirect features

--- src/state/actionCreators/authAC.js
+++ src/state/actionCreators/authAC.js
@@ -1,33 +1,34 @@
-import { apiGetAuth, apiSetAuth } from "../../api";
+import { apiGetAuth, apiSetAuth } from '../../api';
 
 const login = (credentials) => {
-  return async (dispatch) => {
-    let response = await apiSetAuth(credentials);
-    const { token } = response.data;
-    window.localStorage.setItem("token", token);
-    response = await apiGetAuth(token);
-    console.log(response.data);
-    const auth = response.data;
-    dispatch({ type: "SET_AUTH", auth: auth });
-  };
+    return async (dispatch) => {
+        let response = await apiSetAuth(credentials);
+        const { token } = response.data;
+        window.localStorage.setItem('token', token);
+        response = await apiGetAuth(token);
+        console.log(response.data);
+        const auth = response.data;
+        dispatch({ type: 'SET_AUTH', auth: auth });
+    };
 };
 
 const logout = () => {
-  return (dispatch) => {
-    window.localStorage.removeItem("token");
-    dispatch({ auth: {}, type: "SET_AUTH" });
-  };
+    console.log(1);
+    return (dispatch) => {
+        window.localStorage.removeItem('token');
+        dispatch({ auth: {}, type: 'SET_AUTH' });
+    };
 };
 
 const exchangeToken = () => {
-  return async (dispatch) => {
-    const token = window.localStorage.getItem("token");
-    if (token) {
-      const response = await apiGetAuth(token);
-      const auth = response.data;
-      dispatch({ type: "SET_AUTH", auth: auth });
-    }
-  };
+    return async (dispatch) => {
+        const token = window.localStorage.getItem('token');
+        if (token) {
+            const response = await apiGetAuth(token);
+            const auth = response.data;
+            dispatch({ type: 'SET_AUTH', auth: auth });
+        }
+    };
 };
 
 export { login, logout, exchangeToken };
