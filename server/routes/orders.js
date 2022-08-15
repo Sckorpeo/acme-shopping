a015050e332621ff328b1da71b1fd08eea30dec2
Kaiyuan Ma <frankmkmk1@gmail.com>
2022-08-15 11:58:13 -0400

    minor changes

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,29 +1,29 @@
-const express = require("express");
+const express = require('express');
 const app = express.Router();
-const { isLoggedIn } = require("./middleware");
+const { isLoggedIn } = require('./middleware');
 
-app.post("/", isLoggedIn, async (req, res, next) => {
-  try {
-    res.send(await req.user.createOrderFromCart());
-  } catch (ex) {
-    next(ex);
-  }
+app.post('/', isLoggedIn, async (req, res, next) => {
+    try {
+        res.send(await req.user.createOrderFromCart());
+    } catch (ex) {
+        next(ex);
+    }
 });
 
-app.put("/cart", isLoggedIn, async (req, res, next) => {
-  try {
-    res.send(await req.user.addToCart(req.body));
-  } catch (ex) {
-    next(ex);
-  }
+app.put('/cart', isLoggedIn, async (req, res, next) => {
+    try {
+        res.send(await req.user.addToCart(req.body));
+    } catch (ex) {
+        next(ex);
+    }
 });
 
-app.get("/cart", isLoggedIn, async (req, res, next) => {
-  try {
-    res.send(await req.user.getCart());
-  } catch (ex) {
-    next(ex);
-  }
+app.get('/cart', isLoggedIn, async (req, res, next) => {
+    try {
+        res.send(await req.user.getCart());
+    } catch (ex) {
+        next(ex);
+    }
 });
 
 module.exports = app;
