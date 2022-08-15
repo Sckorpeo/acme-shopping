8fd050266492696adfe63f72b0babcc04481749a
Austin Kcon Cheng <austin.kcon.cheng@gmail.com>
2022-08-15 03:43:14 -0700

    added .env to allow my own postgres conn and fixed app bug

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import App from "./app";
+import App from "./App";
 import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
 import store from "./state/store";
