fca71bfc700283fad8475708726e72e8bbc6ae7d
Austin Kcon Cheng <austin.kcon.cheng@gmail.com>
2022-08-16 07:17:01 -0700

    adding redux dev tool

--- src/state/store.js
+++ src/state/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import reducers from "./reducer";
 
-const store = createStore(reducers, {}, applyMiddleware(thunk, logger));
+const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware(thunk, logger)));
 
 export default store;
