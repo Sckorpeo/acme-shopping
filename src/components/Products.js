4320ea974ac39db09e88034825e5069c4573bb90
Kaiyuan Ma <frankmkmk1@gmail.com>
2022-08-15 21:40:00 -0400

    test

--- src/components/Products.js
+++ src/components/Products.js
@@ -7,7 +7,7 @@ import { addToCart } from '../state/actionCreators/cartAC';
 
 function Products(props) {
     const dispatch = useDispatch();
-    const { auth } = useSelector((state) => state.auth);
+    const { auth } = useSelector((state) => state);
     const { products } = useSelector((state) => state.products);
     const categoryId = props.categoryId;
 
