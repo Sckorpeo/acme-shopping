c6c35beedf695fc01c325ba0a898709189b8ace8
Austin Kcon Cheng <austin.kcon.cheng@gmail.com>
2022-08-16 07:04:58 -0700

    use uuid as pk except for category

--- server/db/LineItem.js
+++ server/db/LineItem.js
@@ -2,6 +2,11 @@ const conn = require('./conn');
 const { Sequelize } = conn;
 
 const LineItem = conn.define('lineItem', {
+    id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+    },
     quantity: {
         type: Sequelize.INTEGER,
         defaultValue: 1,
--- server/db/Order.js
+++ server/db/Order.js
@@ -2,6 +2,11 @@ const conn = require('./conn');
 const { Sequelize } = conn;
 
 const Order = conn.define('order', {
+    id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+    },
     isCart: {
         type: Sequelize.BOOLEAN,
         defaultValue: true,
