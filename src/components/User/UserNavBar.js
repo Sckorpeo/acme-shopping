a49e773948ca19d394a2366bee43d6c4af64049f
Kaiyuan Ma <frankmkmk1@gmail.com>
2022-08-21 12:24:36 -0400

    added user page css

--- src/components/User/UserNavBar.js
+++ src/components/User/UserNavBar.js
@@ -1,23 +1,53 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './UserNavBar.css';
-import {logout} from "../../state/actionCreators/authAC";
-import {useDispatch} from "react-redux";
-import {emptyCart} from "../../state/actionCreators/cartAC";
+import { logout } from '../../state/actionCreators/authAC';
+import { useDispatch } from 'react-redux';
+import { emptyCart } from '../../state/actionCreators/cartAC';
 
 function UserNavBar() {
     const dispatch = useDispatch();
     return (
-        <nav className='user-nav-bar'>
-            <NavLink to='info'>User Info</NavLink>
-            <NavLink to='security'>Security</NavLink>
-            <NavLink to='orders'>Past Orders</NavLink>
-            <NavLink onClick={()=>{
-                dispatch(logout());
-                dispatch(emptyCart())
-            }} to='/'>Logout</NavLink>
+        <nav className="user-nav-bar">
+            <NavLink
+                className={({ isActive }) =>
+                    isActive ? 'user-link-active' : 'neumorphism-btn'
+                }
+                to="info"
+            >
+                User Info
+            </NavLink>
+            <hr className="splitter" />
+            <NavLink
+                className={({ isActive }) =>
+                    isActive ? 'user-link-active' : 'neumorphism-btn'
+                }
+                to="security"
+            >
+                Security
+            </NavLink>
+            <hr className="splitter" />
+            <NavLink
+                className={({ isActive }) =>
+                    isActive ? 'user-link-active' : 'neumorphism-btn'
+                }
+                to="orders"
+            >
+                Past Orders
+            </NavLink>
+            <hr className="splitter" />
+            {/* <NavLink
+                className="neumorphism-with-border"
+                onClick={() => {
+                    dispatch(logout());
+                    dispatch(emptyCart());
+                }}
+                to="/"
+            >
+                Logout
+            </NavLink> */}
         </nav>
-    )
-};
+    );
+}
 
 export default UserNavBar;
