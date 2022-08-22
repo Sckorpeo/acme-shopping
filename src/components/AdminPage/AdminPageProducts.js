8756190964aaa17d05915dda0c8775435b45926c
Kaiyuan Ma <frankmkmk1@gmail.com>
2022-08-22 17:23:47 -0400

    finished admin products page

--- src/components/AdminPage/AdminPageProducts.js
+++ src/components/AdminPage/AdminPageProducts.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import {
     apiGetProducts,
     apiAdminDeleteProduct,
     apiAdminEditProduct,
     apiAdminAddProduct,
 } from '../../api/products';
+import './AdminPage.css';
 
 function AdminPageProducts() {
     const [products, setProducts] = useState([]);
@@ -23,8 +25,16 @@ function AdminPageProducts() {
     return (
         <div>
             {products.map((product) => (
-                <p key={product.id}>
-                    {product.name}{' '}
+                <NavLink
+                    className="admin-product-item"
+                    to={`products/${product.id}`}
+                    key={product.id}
+                >
+                    <div>name: {product.name}</div>
+                    <div>price: {product.price}</div>
+                    <div>min players: {product.minPlayers}</div>
+                    <div>max Player: {product.maxPlayers}</div>
+                    <div>time to play: {product.timeToPlay}</div>
                     <button
                         onClick={() => {
                             handleDelete(product.id);
@@ -32,7 +42,7 @@ function AdminPageProducts() {
                     >
                         X
                     </button>
-                </p>
+                </NavLink>
             ))}
         </div>
     );
