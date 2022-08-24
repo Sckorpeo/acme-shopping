81e45fb00a15650734d15a38aba600b85d7ad469
Kaiyuan Ma <frankmkmk1@gmail.com>
2022-08-23 23:31:27 -0400

    added css for product.js

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,40 +4,80 @@ import { useDispatch } from 'react-redux';
 import { apiGetProduct, apiGetProductRatings } from '../../api/products';
 import { addToCart } from '../../state/actionCreators/cartAC';
 import './Product.css';
+import StarRatingReadOnly from '../StarRating/StarRatingReadOnly';
 
 function Product() {
     const { productId } = useParams();
     const [product, setProduct] = useState({});
     const [ratings, setRatings] = useState([]);
+    const [rate, setRate] = useState(0);
     const dispatch = useDispatch();
     async function fetchData() {
         let response = await apiGetProduct(productId);
         setProduct(response.data);
         response = await apiGetProductRatings(productId);
         setRatings(response.data);
+        if (response.data.length > 0) {
+            let sum = 0;
+            for (let e of response.data) {
+                sum += e.value;
+            }
+            const average = sum / response.data.length;
+            setRate(Math.ceil(average));
+        }
     }
     useEffect(() => {
         fetchData();
     }, []);
-
-    console.log(ratings);
     const handleClick = () => {
         dispatch(addToCart({ product, quantity: 1 }));
     };
     return (
         <div className="product-page">
-            <img src={product.imageUrl} />
-            <div className="product-content">
-                <h1>{product.name}</h1>
-                <p>
-                    Pellentesque habitant morbi tristique senectus et netus et
-                    malesuada fames ac turpis egestas. Vestibulum tortor quam,
-                    feugiat vitae, ultricies eget, tempor sit amet, ante. Donec
-                    eu libero sit amet quam egestas semper. Aenean ultricies mi
-                    vitae est. Mauris placerat eleifend leo.
-                </p>
-                <button onClick={handleClick}>Add To Cart</button>
+            <div className="product-product-wrapper">
+                <div className="product-wrapper neumorphism">
+                    <img className="neumorphism-inset" src={product.imageUrl} />
+                    <div className="product-content">
+                        <h1>{product.name}</h1>
+                        <StarRatingReadOnly
+                            rate={rate}
+                            rateCount={ratings.length}
+                        />
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Aliquet nec ullamcorper sit
+                            amet risus nullam. Nunc mi ipsum faucibus vitae
+                            aliquet nec ullamcorper. Arcu odio ut sem nulla
+                            pharetra diam sit amet nisl. Magna etiam tempor orci
+                            eu lobortis elementum. Enim diam vulputate ut
+                            pharetra. Ut sem viverra aliquet eget sit amet
+                            tellus cras adipiscing. Viverra adipiscing at in
+                            tellus integer feugiat scelerisque varius morbi.
+                            Tempor nec feugiat nisl pretium fusce id. Vivamus
+                            arcu felis bibendum ut tristique et egestas quis.
+                            Urna et pharetra pharetra massa massa ultricies mi.
+                        </p>
+                    </div>
+                </div>
+                <div className="product-specific neumorphism">
+                    <div className="product-specific-price">
+                        ${product.price}
+                    </div>
+                    <hr className="splitter" />
+                    <p>Minimum Players: {product.minPlayers}</p>
+                    <p>Maximum Players: {product.maxPlayers}</p>
+                    <p>Estimate Play Time: {product.timeToPlay} min</p>
+                    <button className="neumorphism-btn" onClick={handleClick}>
+                        Add To Cart
+                    </button>
+                </div>
+                <link
+                    rel="stylesheet"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+                />
             </div>
+            <div className="rating-wrapper"></div>
         </div>
     );
 }
