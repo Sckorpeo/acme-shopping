17ab67bc2b8552290255cf9d4407146676ffc13b
Kieran B <kieranbru.web@gmail.com>
2022-08-26 23:39:11 -0400

    Added process webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const DotenvWebpackPlugin = require('dotenv-webpack');
 
 module.exports = merge(common, {
     mode: 'production',
+    plugins: [new DotenvWebpackPlugin()],
 });
