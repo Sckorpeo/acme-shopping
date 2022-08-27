9d05c2b10f50b9900f8bf6cc2d41205f629a6caa
Kieran B <kieranbru.web@gmail.com>
2022-08-26 20:42:40 -0400

    When loggin in, add offline items to cart

--- src/state/actionCreators/authAC.js
+++ src/state/actionCreators/authAC.js
@@ -1,21 +1,27 @@
-import { apiGetAuth, apiSetAuth } from '../../api';
+import { apiGetAuth, apiSetAuth, apiAddToCart } from '../../api';
 
 const login = (credentials) => {
     return async (dispatch) => {
         try {
             let response = await apiSetAuth(credentials);
-            const {token} = response.data;
+            const { token } = response.data;
             window.localStorage.setItem('token', token);
             response = await apiGetAuth(token);
-            console.log(response.data);
             const auth = response.data;
-            dispatch({type: 'SET_AUTH', auth: auth});
+            const guestCart = JSON.parse(window.localStorage.getItem('cart'));
+            if (guestCart?.length) {
+                await Promise.all(
+                    guestCart.map((lineItem) => apiAddToCart(lineItem))
+                );
+            }
+            window.localStorage.removeItem('cart');
+            dispatch({ type: 'SET_AUTH', auth: auth });
         } catch (ex) {
-            alert('Invalid login information!')
+            alert('Invalid login information!');
             console.log(ex);
-            throw ex
+            throw ex;
         }
-    }
+    };
 };
 
 const logout = () => {
