35afec0334fbb3d9c8941f87473920c7d999977f
Kieran B <kieranbru.web@gmail.com>
2022-08-26 22:06:32 -0400

    Updated for another start error

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
-require('dotenv').config();
+if (process.env.NODE_ENV === 'development') {
+    require('dotenv').config();
+}
 const config = {};
 
 if (process.env.QUIET) {
