cffa55881b18b7c645a12bf6cb7e63c10e6f8cc9
Kaiyuan Ma <frankmkmk1@gmail.com>
2023-01-11 17:33:49 -0500

    added session redux store, cookie for jwt (untested)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import App from "./components/App";
-import {Provider} from "react-redux";
-import {BrowserRouter} from 'react-router-dom';
-import {createRoot} from "react-dom/client";
-import store from "./state/store";
+import React from 'react';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import store from './state/store';
 
-const root = createRoot(document.querySelector("#root"));
+const root = createRoot(document.querySelector('#root'));
 
 root.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App/>
+            <App />
         </BrowserRouter>
     </Provider>
 );
